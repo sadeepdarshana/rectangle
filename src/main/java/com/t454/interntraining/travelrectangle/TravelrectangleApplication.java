29ed2be21d59533931bc8178362bc6d997cdbdbb
Sadeep Weerasinghe <sadeepw@codegen.net>
2019-07-01 14:00:32 +0530

    initial commit

---
+++ src/main/java/com/t454/interntraining/travelrectangle/TravelrectangleApplication.java
@@ -0,0 +1,13 @@
+package com.t454.interntraining.travelrectangle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelrectangleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelrectangleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/t454/interntraining/travelrectangle/TravelrectangleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.t454.interntraining.travelrectangle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravelrectangleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
