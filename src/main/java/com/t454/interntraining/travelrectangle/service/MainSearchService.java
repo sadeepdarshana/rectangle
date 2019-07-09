1dc08c1a2a01b67c9afd1ff0a3c868ac51c285af
sadeep <darshanasadeep@gmail.com>
2019-07-09 05:43:23 +0530

    svsdf

--- src/main/java/com/t454/interntraining/travelrectangle/service/MainSearchService.java
+++ src/main/java/com/t454/interntraining/travelrectangle/service/MainSearchService.java
@@ -1,9 +1,6 @@
 package com.t454.interntraining.travelrectangle.service;
 
-import com.t454.interntraining.travelrectangle.model.Contract;
-import com.t454.interntraining.travelrectangle.model.Hotel;
-import com.t454.interntraining.travelrectangle.model.RoomType;
-import com.t454.interntraining.travelrectangle.model.SearchRequest;
+import com.t454.interntraining.travelrectangle.model.*;
 import com.t454.interntraining.travelrectangle.repository.ContractRepository;
 import com.t454.interntraining.travelrectangle.repository.HotelRepository;
 import com.t454.interntraining.travelrectangle.repository.RoomTypeRepository;
@@ -60,12 +57,45 @@ public class MainSearchService {
                 .filter(p->p.getHotelDistrictIndex()==searchDistrict||searchDistrict==-1)
                 .collect(Collectors.toList());
 
-        HashMap<Integer,boolean[]> hotelValidationMatrix = new HashMap<>();
 
+
+        int neededFromEachCapacity[] = new int[]{0,0,0,0};
+        for(CapacityCount cc:searchRequest.getItems()){
+            neededFromEachCapacity[cc.getCapacity()-1]=cc.getCount();
+        }
+
+
+        HashMap<Integer,boolean[]> hotelValidationMatrix = new HashMap<>();
         //1 capacity,2 capacity,3 capacity,4 capacity
         for(int i:hotelIds)hotelValidationMatrix.put(i,new boolean[]{false,false,false,false});
+        HashMap<Integer,RoomType[]> selectedRoomTypes = new HashMap<>();
+        for(int i:hotelIds)selectedRoomTypes.put(i,new RoomType[]{null,null,null,null});
 
-        return new ResponseEntity<>(hotels, HttpStatus.OK);
+
+        for(int cc=0;cc<4;cc++)
+            if(neededFromEachCapacity[cc]==0)for(int i:hotelIds)hotelValidationMatrix.get(i)[cc]=true;
+
+        for(RoomType r:roomTypes){
+            if(r.getQuantity()>=neededFromEachCapacity[r.getRoomTypeCapacity()-1]){
+                selectedRoomTypes.get(r.getHotelId())[r.getRoomTypeCapacity()-1] = r;
+                hotelValidationMatrix.get(r.getHotelId())[r.getRoomTypeCapacity()-1] = true;
+            }
+        }
+
+        ArrayList<Integer>selectedHotelIds = new ArrayList<>();
+
+        for(int i:hotelIds){
+            if(hotelValidationMatrix.get(i)[0]&&
+                    hotelValidationMatrix.get(i)[1]&&
+                    hotelValidationMatrix.get(i)[2]&&
+                    hotelValidationMatrix.get(i)[3]){
+                selectedHotelIds.add(i);
+            }
+        }
+
+
+
+        return new ResponseEntity<>(selectedHotelIds, HttpStatus.OK);
     }
 
 }
